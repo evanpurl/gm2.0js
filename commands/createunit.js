3566d7105621177df68f5889162a7c012018f2d9
evanpurl <evan@purlcomputers.com>
2022-05-23 12:40:57 -0700

    Add files via upload

--- commands/createunit.js
+++ commands/createunit.js
@@ -35,61 +35,65 @@ module.exports = {
                     interaction.reply({content: "You have reached the max number of units.", ephemeral: true });
                     return;
                 } else{
-                    if (fs.existsSync(`./servers/${interaction.guild.id}/dmrole.txt`)) {
-                        file = fs.readFile(`./servers/${interaction.guild.id}/dmrole.txt`, 'utf-8',(err, data)=> {
-                            let myRole = interaction.guild.roles.cache.find(role => role.name === data);
-                            if (myRole) {
-                                const user = guild.members.cache.get(interaction.user.id) || guild.members.cache.fetch(interaction.user.id);
-                                if (user.roles.cache.has(myRole.id)) {
-                                    const unitembed = new MessageEmbed() // Embed Start
-                                        .setColor('#FFFFFF')
-                                        .setTitle('Created Unit')
-                                        .setURL('https://www.nitelifesoftware.com/gmutility/gmunittester.html')
-                                        .setAuthor({name: interaction.user.username})
-                                        .setDescription('Unit you created using the /createunit command.')
-                                        .addFields(
-                                            { name: 'Unit Name:', value: interaction.options.getString('unit_name'), inline: true},
-                                            { name: 'Shields:', value: String(interaction.options.getInteger('shields')), inline: true},
-                                            { name: 'Health:', value: String(interaction.options.getInteger('health')), inline: true},
-                                            { name: 'Shield Damage:', value: String(interaction.options.getInteger('shield_damage')), inline: true},
-                                            { name: 'Damage:', value: String(interaction.options.getInteger('damage')), inline: true},
-                                        )
-                                        .setTimestamp() // Embed end
-                                    try {
-            
-                                        if (!fs.existsSync(`./servers/${interaction.guild.id}/units`)){
-                                            fs.mkdir(`./servers/${interaction.guild.id}/units`, {recursive: true}, (err)=> {
+                    if (interaction.options.getString('unit_name').length > 50){
+                        interaction.reply({content: "The unit you attempted to create has a name over 50 characters, please choose a unit name with a shorter name.", ephemeral: true })
+                    } else {
+                        if (fs.existsSync(`./servers/${interaction.guild.id}/dmrole.txt`)) {
+                            file = fs.readFile(`./servers/${interaction.guild.id}/dmrole.txt`, 'utf-8',(err, data)=> {
+                                let myRole = interaction.guild.roles.cache.find(role => role.name === data);
+                                if (myRole) {
+                                    const user = guild.members.cache.get(interaction.user.id) || guild.members.cache.fetch(interaction.user.id);
+                                    if (user.roles.cache.has(myRole.id)) {
+                                        const unitembed = new MessageEmbed() // Embed Start
+                                            .setColor('#FFFFFF')
+                                            .setTitle('Created Unit')
+                                            .setURL('https://www.nitelifesoftware.com/gmutility/gmunittester.html')
+                                            .setAuthor({name: interaction.user.username})
+                                            .setDescription('Unit you created using the /createunit command.')
+                                            .addFields(
+                                                { name: 'Unit Name:', value: interaction.options.getString('unit_name'), inline: true},
+                                                { name: 'Shields:', value: String(interaction.options.getInteger('shields')), inline: true},
+                                                { name: 'Health:', value: String(interaction.options.getInteger('health')), inline: true},
+                                                { name: 'Shield Damage:', value: String(interaction.options.getInteger('shield_damage')), inline: true},
+                                                { name: 'Damage:', value: String(interaction.options.getInteger('damage')), inline: true},
+                                            )
+                                            .setTimestamp() // Embed end
+                                        try {
+                
+                                            if (!fs.existsSync(`./servers/${interaction.guild.id}/units`)){
+                                                fs.mkdir(`./servers/${interaction.guild.id}/units`, {recursive: true}, (err)=> {
+                                                    if (err){
+                                                        console.log(err);
+                                                    };
+                                                    
+                                                });
+                                            }
+                                            interaction.reply({embeds: [unitembed]});
+                                            fs.writeFile(`./servers/${interaction.guild.id}/units/${interaction.options.getString('unit_name')}.txt`, `shields: ${interaction.options.getInteger('shields')} \nhealth: ${interaction.options.getInteger('health')}\nshield damage: ${interaction.options.getInteger('shield_damage')}\ndamage: ${interaction.options.getInteger('damage')}`, {encoding: 'utf-8', flag: 'w'}, (err)=> {
                                                 if (err){
                                                     console.log(err);
                                                 };
-                                                
                                             });
-                                        }
-                                        interaction.reply({embeds: [unitembed], ephemeral: true });
-                                        fs.writeFile(`./servers/${interaction.guild.id}/units/${interaction.options.getString('unit_name')}.txt`, `shields: ${interaction.options.getInteger('shields')} \nhealth: ${interaction.options.getInteger('health')}\nshield damage: ${interaction.options.getInteger('shield_damage')}\ndamage: ${interaction.options.getInteger('damage')}`, {encoding: 'utf-8', flag: 'w'}, (err)=> {
+                                        } catch(err){
                                             if (err){
                                                 console.log(err);
                                             };
-                                        });
-                                    } catch(err){
-                                        if (err){
-                                            console.log(err);
-                                        };
-                                    }
+                                        }
+                                    } else {
+                                        interaction.reply({content: `You do not have the role **${data}** to use that command.`, ephemeral: true });
+                                        return;
+                                    };
                                 } else {
-                                    interaction.reply({content: `You do not have the role **${data}** to use that command.`, ephemeral: true });
+                                    interaction.reply({content: "DM role does not exist, reconfigure it with /config.", ephemeral: true });
                                     return;
                                 };
-                            } else {
-                                interaction.reply({content: "DM role does not exist, reconfigure it with /config.", ephemeral: true });
-                                return;
-                            };
-                        });
-                        
-                    } else {
-                        interaction.reply({content: "DM role not found, ending command.", ephemeral: true });
-                        return;
-                    };
+                            });
+                            
+                        } else {
+                            interaction.reply({content: "DM role not found, ending command.", ephemeral: true });
+                            return;
+                        };
+                    }
                 }
             });
         }
